00bc9f1cb76904a1f0339bffba2450b562b4f452
ElbekLatifov <latifovelbek02@gmail.com>
2023-06-10 14:15:35 +0500

    Добавьте файлы проекта.

---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,44 @@
+﻿using BackgroundServices.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackgroundServices.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ServicesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Add(int s, int t)
+    {
+        if(!ModelState.IsValid)
+        {
+            return BadRequest("Invalide sonlar");
+        }
+        var job = new Job()
+        {
+            Id = Guid.NewGuid(),
+            A = s,
+            B = t
+        };
+        JobService.Jobs.Add(job);
+
+        return Ok(job.Id);
+    }
+    [HttpPost("result")]
+    public async Task<IActionResult> GetResult(Guid guid)
+    {
+        var job = JobService.Jobs.FirstOrDefault(x => x.Id == guid);
+        if (job == null)
+        {
+            return NotFound();
+        }
+
+        if (!job.IsReady)
+        {
+            return Ok("Bajarilmoqda...");
+        }
+
+        return Ok(job.Result);
+    }
+}
---
+++ Service/ExampleBackgroundService.cs
@@ -0,0 +1,34 @@
+﻿
+namespace BackgroundServices.Services;
+public class ExampleBackgroundService : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken) 
+    {
+        var timer = new Timer(Tick, null, 0, 20000); 
+    }
+
+    public void Tick(object? sender)
+    {
+        var jobs = JobService.Jobs.Where(p=>p.IsReady ==  false).ToList();  
+
+        foreach (var job in jobs)
+        {
+            job.Result = job.A + job.B;
+            job.IsReady = true;
+        }
+    }
+}
+
+public class JobService
+{
+    public static List<Job> Jobs { get; set; } = new List<Job>();
+}
+
+public class Job
+{
+    public Guid Id { get; set; }
+    public int A { get; set; }
+    public int B { get; set; }
+    public int Result { get; set; }
+    public bool IsReady { get; set; }
+}
